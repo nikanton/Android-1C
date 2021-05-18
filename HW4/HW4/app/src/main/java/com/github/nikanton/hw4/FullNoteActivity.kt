6fe703cdcb8afa29053b1fcfe9568613a82dbc99
Anton Nikishin <nikanton@live.ru>
2021-05-18 23:17:45 +0300

    HW4

---
+++ HW4/HW4/app/src/main/java/com/github/nikanton/hw4/FullNoteActivity.kt
@@ -0,0 +1,11 @@
+package com.github.nikanton.hw4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FullNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_full_note)
+    }
+}
